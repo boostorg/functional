de1bf61e1386ca57c0798dfa7a02ce59d2f45612
Denis Mikhailov <denismikhaylov38@gmail.com>
2022-01-12 09:08:20 +0400

    Update identity.hpp

--- include/boost/functional/identity.hpp
+++ include/boost/functional/identity.hpp
@@ -17,13 +17,7 @@ namespace boost {
 
 struct identity {
     typedef void is_transparent;
-
-#if !defined(BOOST_NO_CXX11_RVALUE_REFERENCES)
-    template<class T>
-    BOOST_CONSTEXPR T&& operator()(T&& value) const BOOST_NOEXCEPT {
-        return std::forward<T>(value);
-    }
-#else
+    
     template<class> struct result;
 
     template<class F, class T>
@@ -36,6 +30,32 @@ struct identity {
         typedef T& type;
     };
 
+#if !defined(BOOST_NO_CXX11_RVALUE_REFERENCES)
+    template<class F, class T>
+    struct result<F(T)> {
+        typedef T&& type;
+    };
+
+    template<class F, class T>
+    struct result<F(const T&&)> {
+        typedef const T&& type;
+    };
+
+    template<class F, class T>
+    struct result<F(T&&)> {
+        typedef T&& type;
+    };
+
+    template<class T>
+    BOOST_CONSTEXPR T&& operator()(T&& value) const BOOST_NOEXCEPT {
+        return std::forward<T>(value);
+    }
+#else
+    template<class F, class T>
+    struct result<F(T)> {
+        typedef const T& type;
+    };
+    
     template<class T>
     BOOST_CONSTEXPR const T& operator()(const T& value) const BOOST_NOEXCEPT {
         return value;
